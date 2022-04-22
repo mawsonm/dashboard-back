4614139fe3be5d3c8926c7fb516a68043eb667f9
mawsonm <mawsonm@umich.edu>
2022-04-21 22:56:11 -0400

    initial project

---
+++ src/main/java/com/mydash/dashboard/SpringBootDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.mydash.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDashboardApplication.class, args);
+	}
+
+}
